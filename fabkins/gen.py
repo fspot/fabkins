d6e7aa6a9a13a0e1e3cdde4d861feeafb281b0f0
fred <fred.matigot@gmail.com>
2013-08-23 17:01:55 +0200

    gen functions

--- fabkins/gen.py
+++ fabkins/gen.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python
 # -*- coding:utf_8 -*-
 
+import os
+import shutil
+
 import clize
 
+import fabkins
+
 
 @clize.clize
-def conf():
-    pass
+def conf(dest="fabkins.ini"):
+    """ Generate a default configuration file for fabkins. """
+    open(dest, "w")
 
 @clize.clize
-def static():
-    pass
+def static(dest="static"):
+    """ Generate the static files directory used by fabkins. """
+    static_dir = os.path.join(fabkins.__path__[0], 'static')
+    shutil.copytree(static_dir, dest)
 
 
 def conf_entry_point():
-    conf()
+    import sys
+    try:
+       conf(*sys.argv)
+    except clize.ArgumentError:
+       conf(sys.argv[0], '-h')
 
 def static_entry_point():
-    static()
+    import sys
+    try:
+        static(*sys.argv)
+    except clize.ArgumentError:
+        static(sys.argv[0], '-h')
 
